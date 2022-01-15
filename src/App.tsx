1bf2b644f71f83169c59ec1b224378623a5aeeec
Mateusz Lisowski <lisina091@gmail.com>
2022-01-15 19:42:35 +0100

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Display from "./components/Display";
 import { Provider } from 'react-redux';
-import { store } from './redux';
+import { store } from './redux/store';
 
 
 function App() {
--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
-import { RootState } from "../redux";
+import { RootState } from "../redux/store";
 import { GetHero } from "../redux/actions/HeroActions";
 
 
